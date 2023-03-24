351f3e393b3b9abf34ad0a6703bb45fc2dc61454
nineuito <nineuito@gmail.com>
2023-03-24 20:23:31 +0200

    revision

--- scripts/main.js
+++ scripts/main.js
@@ -6,5 +6,20 @@ document.addEventListener("keydown", function(event) {
 });
 
 function jump () {
-    person.classList.add('jump');
+    if (person.classList != 'jump') {
+        person.classList.add('jump')
+    }
+    setTimeout(() => {
+        person.classList.remove('jump');
+    }, 300);
 }
+
+let isAlive = setInterval ( function () {
+    let personTop = parseInt(window.getComputedStyle(person).getPropertyValue('top'));
+    let cactusLeft = parseInt(window.getComputedStyle(cactus).getPropertyValue('left'));
+    
+    
+    if (cactusLeft < 50 && cactusLeft > 0 && personTop >= 140) {
+        alert('Game over!!');
+    }
+})
